2a2ead639cb70776c382e772d10f25d061680e49
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-07-15 12:24:52 +0600

    thirt commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {DataProvider} from './GlobalState'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { DataProvider } from './GlobalState'
 import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
 
@@ -10,8 +10,12 @@ function App() {
     <DataProvider>
       <Router>
         <div className="App">
-          <Header />
-          <MainPages />
+
+          <Route exact path="/">
+            <Header />
+            <MainPages />
+          </Route>
+
         </div>
       </Router>
     </DataProvider>
