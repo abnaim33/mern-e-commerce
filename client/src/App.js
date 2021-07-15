85c64dce93adf582576e6128a6e96393296dc93e
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-07-15 12:31:04 +0600

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { DataProvider } from './GlobalState'
 import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
